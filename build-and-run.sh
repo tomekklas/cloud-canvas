947216191203e9bfa0d21902b3560c62cd9810ff
Tomek Klas <github@tomekklas.com>
2023-10-30 19:51:27 +0000

    refactored so lambdas are now in separate directory and are not duplicated on each build. some cleanup in codebuild needed.

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,4 +2,4 @@
 
 ./codebuild_build.sh -a /tmp -i public.ecr.aws/codebuild/amazonlinux2-x86_64-standard:5.0
 
-aws cloudformation update-stack --stack-name deployment --template-url  https://deployment-idea-test-bucket.s3.eu-central-1.amazonaws.com/artefacts/xxx/v1/deployment.yaml --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --parameters ParameterKey=Environment,ParameterValue=Dev
+# aws cloudformation update-stack --stack-name deployment --template-url  https://deployment-idea-test-bucket.s3.eu-central-1.amazonaws.com/artefacts/xxx/v1/deployment.yaml --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --parameters ParameterKey=Environment,ParameterValue=Dev
