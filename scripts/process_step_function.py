a8369813e161fd2fd9c01f2d5b116ebbfe87aa14
Tomek Klas <github@tomekklas.com>
2023-12-28 19:53:50 +0000

    generated multiple map SF, needs more work

--- scripts/process_step_function.py
+++ scripts/process_step_function.py
@@ -1,6 +1,9 @@
 import os
 import yaml
 import json
+import copy
+import shutil
+import tempfile
 from collections import OrderedDict
 from jinja2 import Environment, FileSystemLoader
 
@@ -29,44 +32,11 @@ def parse_config_files():
                         try:
                             config = yaml.safe_load(f)
 
-                            # Retrieve the required information
-                            add_deployment_setup = config.get('add_deployment_setup', '')
-                            copyright_license = config.get('copyright_license', '')
-                            deployment_default_role_to_assume = config.get('deployment_default_role_to_assume', '')
-                            deployment_dev_bucket = config.get('deployment_dev_bucket', '')
-                            deployment_prod_bucket = config.get('deployment_prod_bucket', '')
-                            deployment_stack_name_prefix = config.get('deployment_stack_name_prefix', '')
-                            include_lambda_scaffolding = config.get('include_lambda_scaffolding', '')
-                            module_directory_name = config.get('module_directory_name', '')
-                            module_execution_order = config.get('module_execution_order', '')
-                            module_name = config.get('module_name', '')
-                            module_type = config.get('module_type', '')
-                            owner_email = config.get('owner_email', '')
-                            owner_name = config.get('owner_name', '')
-                            short_module_description = config.get('short_module_description', '')
+                            # Retrieve the required information and organize data by module_execution_order
+                            if config.get('module_execution_order') not in parsed_data:
+                                parsed_data[config['module_execution_order']] = []
 
-
-
-                            # Organize data by module_execution_order
-                            if module_execution_order not in parsed_data:
-                                parsed_data[module_execution_order] = []
-
-                            parsed_data[module_execution_order].append({
-                                'module_name': module_name,
-                                'module_directory_name': module_directory_name,
-                                'module_type': module_type,
-                                'add_deployment_setup': add_deployment_setup,
-                                'copyright_license': copyright_license,
-                                'deployment_default_role_to_assume': deployment_default_role_to_assume,
-                                'deployment_dev_bucket': deployment_dev_bucket,
-                                'deployment_prod_bucket': deployment_prod_bucket,
-                                'deployment_stack_name_prefix': deployment_stack_name_prefix,
-                                'include_lambda_scaffolding': include_lambda_scaffolding,
-                                'module_execution_order': module_execution_order,
-                                'owner_email': owner_email,
-                                'owner_name': owner_name,
-                                'short_module_description': short_module_description
-                            })
+                            parsed_data[config['module_execution_order']].append(config)
 
                         except yaml.YAMLError as exc:
                             print(f"Error parsing YAML file {file_path}: {exc}")
@@ -77,58 +47,64 @@ def parse_config_files():
     return json.dumps(sorted_data, indent=4)
 
 def create_step_function_file(json_data, templates_dir, output_file):
-    # Load the JSON data
-    modules = json.loads(json_data)
+    # Create a temporary directory
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        # Copy template files to the temporary directory
+        for filename in os.listdir(templates_dir):
+            shutil.copy(os.path.join(templates_dir, filename), tmp_dir)
 
-    # Set up Jinja environment
-    env = Environment(loader=FileSystemLoader(templates_dir))
+        # Load the JSON data
+        modules = json.loads(json_data)
 
-    # Start with the 'beginning.yaml'
-    with open(os.path.join(templates_dir, 'beginning.yaml'), 'r') as file:
-        content = file.read()
+        # Set up Jinja environment to use the temporary directory
+        env = Environment(loader=FileSystemLoader(tmp_dir), cache_size=0)
 
-    # Process each module
-    first_iteration = True  # Variable to track the first iteration
-    for key, module_list in modules.items():
-        for module in module_list:
-            # Determine the template name based on the module type
-            template_name = f"{module['module_type'].lower()}.yaml"
+        # Initialize content outside the loop
+        content = ""
 
-            try:
-                # Get the template from the Jinja2 environment
-                template = env.get_template(template_name)
+        # Start with the 'beginning.yaml'
+        with open(os.path.join(tmp_dir, 'beginning.yaml'), 'r') as file:
+            content = file.read()
 
-                # Add a flag to the module data to indicate the first iteration
-                module['is_first_iteration'] = first_iteration
+        # Process each module
+        first_iteration = True
+        for key, module_list in modules.items():
+            for module in module_list:
+                # Determine the template name based on the module type
+                template_name = f"{module['module_type'].lower()}.yaml"
 
-                # Render the template with the module data
-                rendered_content = template.render(module)
+                try:
+                    # Get the template from the Jinja2 environment
+                    template = env.get_template(template_name)
 
-                # Append the rendered content to the main content
-                content += rendered_content
+                    # Create a deep copy of the module to prevent modification
+                    module_copy = copy.deepcopy(module)
+                    module_copy['is_first_iteration'] = first_iteration
 
-                # Set first_iteration to False after the first module is processed
-                first_iteration = False
+                    # Render the template with the copied module data
+                    rendered_content = template.render(module_copy)
 
-            except jinja2.TemplateNotFound:
-                print(f"Template not found: {template_name}")
-            except jinja2.TemplateError as e:
-                print(f"Template error in {template_name}: {e}")
+                    # Append the rendered content to the main content
+                    content += rendered_content
 
+                    # Set first_iteration to False after the first module is processed
+                    first_iteration = False
 
-    # Append 'end.yaml'
-    with open(os.path.join(templates_dir, 'end.yaml'), 'r') as file:
-        content += file.read()
+                except jinja2.TemplateNotFound:
+                    print(f"Template not found: {template_name}")
+                except jinja2.TemplateError as e:
+                    print(f"Template error in {template_name}: {e}")
 
-    # Save the final content to 'step_function.yaml'
-    with open(output_file, 'w') as file:
-        file.write(content)
+        # Append 'end.yaml'
+        with open(os.path.join(tmp_dir, 'end.yaml'), 'r') as file:
+            content += file.read()
 
+        # Save the final content to the specified output file
+        with open(output_file, 'w') as file:
+            file.write(content)
 
+# Example usage
 json_data = parse_config_files()
-print(json_data)
-
 templates_dir = '_deployment/templates/.step_function_snippets/'
 output_file = '_deployment/templates/step_function_definition.yaml'
-
 create_step_function_file(json_data, templates_dir, output_file)
